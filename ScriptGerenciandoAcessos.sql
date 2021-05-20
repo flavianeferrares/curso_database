f679405088cc0101ece8a1c0cf0b90dfe28035a2
Flaviane Vitoria  Ferrares <43713852+flavianeferrares@users.noreply.github.com>
2021-05-20 08:53:40 -0300

    Add files via upload

---
+++ ScriptGerenciandoAcessos.sql
@@ -0,0 +1,32 @@
+/* 
+Endereço de IP fixo
+	CREATE USER 'andre'@'200.200.190.190'IDENTIFIED BY 'milani123456';
+Qualquer endereço de IP
+	CREATE USER 'andre'@'%'IDENTIFIED BY 'milani123456';
+*/
+
+CREATE USER 'andre'@'localhost' IDENTIFIED BY 'milani123456';
+GRANT ALL ON curso_sql.* TO 'andre'@'localhost';
+
+CREATE USER 'andre'@'%' IDENTIFIED BY 'andreviagem';
+GRANT SELECT ON curso_sql.* TO 'andre'@'%';
+GRANT INSERT ON curso_sql.* TO 'andre'@'%';
+GRANT INSERT ON curso_sql.funcionarios TO 'andre'@'%';
+
+REVOKE INSERT ON curso_sql.funcionarios FROM 'andre'@'%';
+REVOKE SELECT ON curso_sql.* FROM 'andre'@'%';
+
+GRANT SELECT ON curso_sql.funcionarios TO 'andre'@'%';
+GRANT SELECT ON curso_sql.veiculos TO 'andre'@'%';
+
+REVOKE SELECT ON curso_sql.funcionarios FROM 'andre'@'%';
+REVOKE SELECT ON curso_sql.veiculos FROM 'andre'@'%';
+
+DROP USER 'andre'@'%';
+DROP USER 'andre'@'localhost';
+
+SELECT User FROM mysql.user;
+SHOW GRANTS FOR 'andre'@'%';
+
+
+
